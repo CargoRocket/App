6600ce549c8d5efdedbec02f3a93ffe4f6347da6
David Prenninger <david.prenninger@gmx.de>
2021-03-20 14:56:29 +0100

    [All Platforms] Adding first view.

---
+++ metro.config.js
@@ -0,0 +1,25 @@
+const MetroConfig = require('@ui-kitten/metro-config');
+const defaultConfig = require('metro-config/src/defaults').getDefaultValues();
+
+/**
+ * @see https://akveo.github.io/react-native-ui-kitten/docs/guides/improving-performance
+ */
+const evaConfig = {
+  evaPackage: '@eva-design/eva',
+};
+
+module.exports = MetroConfig.create(evaConfig, {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: defaultConfig.resolver.assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
+  },
+});
---
+++ src/App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the UI Kitten template
+ * https://github.com/akveo/react-native-ui-kitten
+ *
+ * Documentation: https://akveo.github.io/react-native-ui-kitten/docs
+ *
+ * @format
+ */
+
+import React from 'react';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import * as eva from '@eva-design/eva';
+import {default as theme} from './res/custom-theme.json';
+import {StartView} from './views/StartView';
+
+/**
+ * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
+ * https://akveo.github.io/eva-icons
+ */
+
+export default () => (
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={{...eva.light, ...theme}}>
+      <StartView />
+    </ApplicationProvider>
+  </>
+);
