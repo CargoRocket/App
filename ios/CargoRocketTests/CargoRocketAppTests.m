a44128affe001938ca5a7ab184b695262d9866a2
David Prenninger <david.prenninger@gmx.de>
2021-05-15 16:23:11 +0200

    [IOS] Fix Ios renaming

--- ios/CargoRocketTests/CargoRocketAppTests.m
+++ ios/CargoRocketTests/CargoRocketAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface CargoRocketAppTests : XCTestCase
+@interface CargoRocketTests : XCTestCase
 
 @end
 
-@implementation CargoRocketAppTests
+@implementation CargoRocketTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
